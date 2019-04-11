0afb1d3ba1bf421a88a139c389aa81e0c2ca4f5d
mahou <tyanmahou@gmail.com>
2019-04-12 02:17:17 +0900

    fix

--- app/utils/Request.php
+++ app/utils/Request.php
@@ -6,12 +6,12 @@ class Request
 {
     static public function getBody(array $requires = []) : array
     {
-        $body = file_get_contents('php://input');
+        $body = json_decode(file_get_contents('php://input'), true);
         foreach ($requires as $value) {
             if (!isset($body['values'][$value])) {
                 throw new RequestException("request requires {$value}");
             }
         }
-        return $body['values'];
+        return $body['values'] ?? [];
     }
 }
